9ae68e84102f33f990e3f1b8860a1877fffa4038
Royweru <royezi17@gmail.com>
2023-12-23 10:47:40 +0300

    modified:   base/__pycache__/models.cpython-311.pyc 	new file:   base/migrations/0002_bodytype_make_remove_model_image_alter_model_name.py 	new file:   base/migrations/__pycache__/0002_bodytype_make_remove_model_image_alter_model_name.cpython-311.pyc 	modified:   base/models.py 	modified:   db.sqlite3

--- base/models.py
+++ base/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 
 # Create your models here.
-class Model(models.Model):
+class Make(models.Model):
     name=models.CharField(max_length=50)
     image = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=None)
+    
+class BodyType(models.Model):
+    name= models.CharField(max_length=100)
+    image = models.ImageField( upload_to=None, height_field=None, width_field=None, max_length=None)
+    
+class Model(models.Model):
+     name= models.CharField(max_length=100)
+    
