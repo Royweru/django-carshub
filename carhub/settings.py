4f3fa944896991b46a036cecc475b5ba7ff18f53
Royweru <royezi17@gmail.com>
2023-12-23 14:29:10 +0300

    modified:   carhub/__pycache__/settings.cpython-311.pyc 	modified:   carhub/settings.py 	modified:   db.sqlite3 	deleted:    media/images/media/images/body/hatchback.png 	deleted:    media/images/media/images/body/pickup.png 	deleted:    media/images/media/images/body/sedan.png 	deleted:    media/images/media/images/body/suv.png 	deleted:    media/images/media/images/body/toyota_logo.jpeg

--- carhub/settings.py
+++ carhub/settings.py
@@ -129,8 +129,8 @@ STATICFILES_DIRS=[
 # settings.py
 
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/images')
-MEDIA_URL = '/media/images/'
+MEDIA_ROOT = ''
+MEDIA_URL = ''
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
