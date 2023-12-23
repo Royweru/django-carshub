c500be21b423a2c63d0adc42eb80c25608227bd9
Royweru <royezi17@gmail.com>
2023-12-23 10:52:23 +0300

    modified:   base/admin.py 	modified:   base/models.py 	deleted:    media/images/None/a51.jpeg

--- base/admin.py
+++ base/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Model
+from .models import Model,Make,BodyType
 
 admin.site.register(Model)
+admin.site.register(Make)
+admin.site.register(BodyType)
--- base/models.py
+++ base/models.py
@@ -3,11 +3,11 @@ from django.db import models
 # Create your models here.
 class Make(models.Model):
     name=models.CharField(max_length=50)
-    image = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=None)
+    image = models.ImageField(upload_to='/media/images/make', height_field=None, width_field=None, max_length=None)
     
 class BodyType(models.Model):
     name= models.CharField(max_length=100)
-    image = models.ImageField( upload_to=None, height_field=None, width_field=None, max_length=None)
+    image = models.ImageField( upload_to='/media/images/body', height_field=None, width_field=None, max_length=None)
     
 class Model(models.Model):
      name= models.CharField(max_length=100)
