c0268a0273c365ce087bee026fee7fcd19518082
Royweru <royezi17@gmail.com>
2023-12-23 10:09:34 +0300

    modified:   base/models.py 	modified:   carhub/urls.py 	modified:   db.sqlite3 	modified:   templates/index.html

--- base/models.py
+++ base/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Model(models.Model):
+    name=models.CharField(max_lenth=50)
+    image = models.ImageField(_("Image"), upload_to=None, height_field=None, width_field=None, max_length=None)
