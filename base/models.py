4fddf98b539572e79108b898ba867851aff8240f
Royweru <royezi17@gmail.com>
2023-12-23 10:21:10 +0300

    modified:   Pipfile 	modified:   Pipfile.lock 	modified:   base/__pycache__/admin.cpython-311.pyc 	modified:   base/__pycache__/models.cpython-311.pyc 	modified:   base/admin.py 	new file:   base/migrations/0001_initial.py 	new file:   base/migrations/__pycache__/0001_initial.cpython-311.pyc 	modified:   base/models.py 	modified:   carhub/__pycache__/settings.cpython-311.pyc 	modified:   carhub/__pycache__/urls.cpython-311.pyc 	modified:   db.sqlite3

--- base/models.py
+++ base/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 # Create your models here.
 class Model(models.Model):
-    name=models.CharField(max_lenth=50)
+    name=models.CharField(max_length=50)
     image = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=None)
